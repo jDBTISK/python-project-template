16e51b88290cb450039c20e4f2e474a63767207d
jDBTISK <de7aef3fd77mt6@gmail.com>
2022-04-18 14:41:42 +0900

    [init] first commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+def add(x: int, y: int) -> int:
+    """
+    整数の加算結果を返却
+
+    Parameters
+    ----------
+    x : int
+        加算対象整数1
+    y : int
+        加算対象整数2
+
+    Returns
+    -------
+    int
+        加算結果整数
+    """
+    return x + y
---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+sys.path.append(
+    os.path.abspath(f"{os.path.dirname(os.path.abspath(__file__))}/../src/")
+)
